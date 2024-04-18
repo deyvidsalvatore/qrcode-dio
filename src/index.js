0fd31d588b5e9a13eaad57168e28288f58279355
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-04-18 01:02:01 -0300

    Password

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import prompt from 'prompt';
 import promptSchemaMain from './prompts-schema/prompt-schema-main.js';
+import createPassword from './services/password/create.js';
 
 async function main() {
     prompt.get(promptSchemaMain, async (err, choose) => {
         if (err) console.log(err);
+
+        if (choose.select == 2) await createPassword();
     });
 
     prompt.start();
