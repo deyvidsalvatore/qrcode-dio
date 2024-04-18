b27319a5ba44fa67c58d50f8b91b7e8c0997842f
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-04-18 01:06:23 -0300

    QR Code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import prompt from 'prompt';
 import promptSchemaMain from './prompts-schema/prompt-schema-main.js';
 import createPassword from './services/password/create.js';
+import createQRCode from './services/qrcode/create.js';
 
 async function main() {
     prompt.get(promptSchemaMain, async (err, choose) => {
         if (err) console.log(err);
 
+        if (choose.select == 1) await createQRCode()
         if (choose.select == 2) await createPassword();
     });
 
